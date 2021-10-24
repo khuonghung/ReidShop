9e4818a416bb4c315740dc9e596f81f8560b345c
Khuong Hung <khuonghung1423@gmail.com>
2021-10-25 01:32:37 +0700

    Reid: push module product page

---
+++ src/java/Controller/Home/Product.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.Home;
+
+import DAO.productDAO;
+import Entity.Category;
+import Entity.Color;
+import Entity.Size;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Khuong Hung
+ */
+public class Product extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String action = request.getParameter("action");
+        if (action == null) {
+            productDAO c = new productDAO();
+            List<Entity.Product> productList = c.getProduct();
+            List<Category> category = c.getCategory();
+            int page, numperpage = 9;
+            int size = productList.size();
+            int num = (size % 9 == 0 ? (size / 9) : ((size / 9)) + 1);//so trang
+            String xpage = request.getParameter("page");
+            if (xpage == null) {
+                page = 1;
+            } else {
+                page = Integer.parseInt(xpage);
+            }
+            int start, end;
+            start = (page - 1) * numperpage;
+            end = Math.min(page * numperpage, size);
+            List<Entity.Product> product = c.getListByPage(productList, start, end);
+            request.setAttribute("page", page);
+            request.setAttribute("num", num);
+            request.setAttribute("CategoryData", category);
+            request.setAttribute("ProductData", product);
+            request.getRequestDispatcher("shop_category.jsp").forward(request, response);
+        }
+        
+        if(action.equalsIgnoreCase("listByCategory")){
+            String category_id = request.getParameter("category_id");
+            productDAO c = new productDAO();
+            List<Entity.Product> productList = c.getProductByCategory(category_id);
+            List<Category> category = c.getCategory();
+            int page, numperpage = 9;
+            int size = productList.size();
+            int num = (size % 9 == 0 ? (size / 9) : ((size / 9)) + 1);//so trang
+            String xpage = request.getParameter("page");
+            if (xpage == null) {
+                page = 1;
+            } else {
+                page = Integer.parseInt(xpage);
+            }
+            int start, end;
+            start = (page - 1) * numperpage;
+            end = Math.min(page * numperpage, size);
+            List<Entity.Product> product = c.getListByPage(productList, start, end);
+            request.setAttribute("page", page);
+            request.setAttribute("num", num);
+            request.setAttribute("CategoryData", category);
+            request.setAttribute("ProductData", product);
+            request.getRequestDispatcher("shop_category.jsp").forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/java/DAO/productDAO.java
+++ src/java/DAO/productDAO.java
@@ -114,7 +114,8 @@ public class productDAO {
         } catch (Exception e) {
             System.out.println(e);
         }
-        
+
+
         String sq3 = "delete from product where product_id=?";
         try {
             conn = new DBContext().getConnection();
@@ -125,6 +126,68 @@ public class productDAO {
             System.out.println(e);
         }
 
+    }
+
+    public void updateProduct(Product product) {
+
+        String sql2 = "delete from product_size where product_id=?";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql2);
+            ps.setString(1, product.getProduct_id());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        String sql3 = "delete from product_color where product_id=?";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql3);
+            ps.setString(1, product.getProduct_id());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        String sql4 = "insert into product_size (product_id,size) values(?,?)";
+        try {
+            conn = new DBContext().getConnection();
+            for (Size i : product.getSize()) {
+                ps = conn.prepareStatement(sql4);
+                ps.setString(1, i.getProduct_id());
+                ps.setString(2, i.getSize());
+                ps.executeUpdate();
+            }
+        } catch (Exception e) {
+        }
+
+        String sql5 = "insert into product_color (product_id,color) values(?,?)";
+        try {
+            conn = new DBContext().getConnection();
+            for (Color i : product.getColor()) {
+                ps = conn.prepareStatement(sql5);
+                ps.setString(1, i.getProduct_id());
+                ps.setString(2, i.getColor());
+                ps.executeUpdate();
+            }
+        } catch (Exception e) {
+        }
+
+        String sql = "update product set category_id=? ,product_name=? ,product_price=? ,product_describe=? ,quantity=? ,img=? where product_id=?";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, product.getCate().getCategory_id());
+            ps.setString(2, product.getProduct_name());
+            ps.setFloat(3, product.getProduct_price());
+            ps.setString(4, product.getProduct_describe());
+            ps.setInt(5, product.getQuantity());
+            ps.setString(6, product.getImg());
+            ps.setString(7, product.getProduct_id());
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
     }
 
     public List<Size> getSize() {
@@ -196,6 +259,49 @@ public class productDAO {
             ps.executeUpdate();
         } catch (Exception e) {
         }
+    }
+
+    public List<Product> getTop10Product() {
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT TOP 10 p.product_id , p.product_name, p.product_price, p.product_describe, p.quantity,p.img FROM product p ORDER BY NEWID()";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getString(1), rs.getString(2), rs.getFloat(3), rs.getString(4), rs.getInt(5), rs.getString(6)));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return list;
+    }
+
+    public List<Product> getListByPage(List<Product> list,
+            int start, int end) {
+        ArrayList<Product> arr = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            arr.add(list.get(i));
+        }
+        return arr;
+    }
+
+    public List<Product> getProductByCategory(String category_id) {
+        List<Product> list = new ArrayList<>();
+        String sql = "select c.category_name , p.product_id , p.product_name, p.product_price, p.product_describe, p.quantity,p.img from product p inner join category c on p.category_id = c.category_id WHERE p.category_id=?";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, category_id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Category c = new Category(rs.getString(1));
+                list.add(new Product(c, rs.getString(2), rs.getString(3), rs.getFloat(4), rs.getString(5), rs.getInt(6), rs.getString(7)));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return list;
     }
 
 }
